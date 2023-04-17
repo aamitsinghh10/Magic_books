8c519dbc7b70fd8cc9a64a059e18e6ed9c679008
Amit Singh <47208399+aamitsinghh10@users.noreply.github.com>
2023-04-18 01:48:39 +0530

    Delete MagicBooks/MagicBooks directory

--- MagicBooks/MagicBooks/src/main/java/com/springboot/magicBooks/database/AdminDatabase.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.magicBooks.database;
-
-import com.springboot.magicBooks.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminDatabase extends JpaRepository<Admin, String> {
-}
